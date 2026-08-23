6fa412d93aa062c35ccc8305f873b89437bb6689
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-23 18:48:21 +0600

    show country details

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({ country }) => {
+    console.log(country)
     return (
-       <div className='country'>
-            <h2>Name:</h2>
+        <div className='country'>
+            <h2>Name:{country.name}</h2>
+            <h2>Population:{country.population}</h2>
+            <h2>Flags:{country.flags.svg}</h2>
         </div>
     );
 };
