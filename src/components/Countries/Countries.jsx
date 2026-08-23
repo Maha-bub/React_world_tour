de6f410d1311e3df711be564a56d06f267096bf1
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-23 17:40:37 +0600

    create a new react project for my practice purpose

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    return (
+        <div>
+            <h3>Countries:</h3>
+        </div>
+    );
+};
+
+export default Countries;
