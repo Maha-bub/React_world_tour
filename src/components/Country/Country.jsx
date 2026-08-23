b72cbfb88dabc91625f68bb4d623ea60a2fc983f
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-23 19:00:53 +0600

    distructuring country information

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -3,11 +3,12 @@ import './Country.css'
 
 const Country = ({ country }) => {
     console.log(country)
+    const { name, flags, population } = country;
     return (
         <div className='country'>
-            <h2>Name:{country.name}</h2>
-            <h2>Population:{country.population}</h2>
-            <h2>Flags:{country.flags.svg}</h2>
+            <h2>Name:{name}</h2>
+            <h2>Population:{population}</h2>
+            <img src={flags?.svg} alt={name?.common} />
         </div>
     );
 };
