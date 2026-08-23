cbd262c37cec3ce95bf2f27c2465f7c176052b2b
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-23 21:26:58 +0600

    check where visited and where planed to go

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
 const Country = ({ country }) => {
     console.log(country)
-    const { name, flags, population } = country;
+    const { name, flags, population, area } = country;
+    const [visited, setVisited] = useState(false);
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
     return (
         <div className='country'>
             <h2>Name:{name}</h2>
-            <h2>Population:{population}</h2>
             <img src={flags?.svg} alt={name?.common} />
+            <h3>Population:{population}</h3>
+            <p>Area:{area}</p>
+            <button onClick={handleVisited}>{visited ? "Visited" : "Going"}</button>
+
         </div>
     );
 };
