4c3f4160aa83b85f454a09fcd39b629f28d7f7c8
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-24 22:22:30 +0600

    create a function handle visited country

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,6 +4,8 @@ import './Countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([]);
 
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
     useEffect(() => {
         fetch('https://countries.dev/countries')
             .then(response => response.json())
@@ -11,14 +13,29 @@ const Countries = () => {
 
     }, [])
 
+
+    const handleVisitedCountry = country => {
+        console.log('add this to your visited list!')
+        console.log(country)
+    }
+
     return (
         <div>
             <h3>Countries:{countries.length}</h3>
+            <div>
+                <h3>Visited Country:</h3>
+                <ul>
+
+                </ul>
+            </div>
             <div className="country-container">
                 {
                     countries.map(country =>
                         <Country
-                            country={country} key={country.numericCode}>
+                            country={country}
+                            key={country.numericCode}
+                            handleVisitedCountry={handleVisitedCountry}
+                        >
 
                         </Country>)
                 }
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
     console.log(country)
     const { name, flags, population, area } = country;
     const [visited, setVisited] = useState(false);
     const handleVisited = () => {
         setVisited(!visited);
     }
+    console.log(handleVisitedCountry)
     return (
-        <div className='country'>
+        <div className={`country ${visited && "visited-country"}`}>
             <h2>Name:{name}</h2>
+
+
             <img src={flags?.svg} alt={name?.common} />
             <h3>Population:{population}</h3>
             <p>Area:{area}</p>
+            <button onClick={handleVisitedCountry}>Mark as Visited</button>
             <button onClick={handleVisited}>{visited ? "Visited" : "Going"}</button>
 
         </div>
